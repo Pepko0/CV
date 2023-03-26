dcd8f27aff6721c52618c729fb7135d5005cb4df
Kacper Lewko <k.lewkosga@gmail.com>
2023-03-26 13:57:41 +0200

    added button Change backgrounnd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const buttonChangeColor = document.querySelector(".js-button");
 const body = document.querySelector(".body");
+const otherName = document.querySelector(".otherName")
+
 
 buttonChangeColor.addEventListener("click", () => {
-    body.classList.toggle("body__dark")
+    body.classList.toggle("body__dark");
+
+    if(body.classList.contains("body__dark") ? otherName.innerText = "Przełącz na jasny motyw" : otherName.innerText = "Przełącz na ciemny motyw");
 });
