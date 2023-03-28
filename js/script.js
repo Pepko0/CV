da29da23f168c41384e0b8431dbd4236128cafda
Kacper Lewko <k.lewkosga@gmail.com>
2023-03-28 12:59:13 +0200

    added media to footer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const buttonChangeColor = document.querySelector(".js-button");
 const body = document.querySelector(".body");
-const otherName = document.querySelector(".otherName")
+const otherName = document.querySelector(".otherName");
+const slider = document.querySelector(".switch");
 
 
 buttonChangeColor.addEventListener("click", () => {
     body.classList.toggle("body__dark");
 
     if(body.classList.contains("body__dark") ? otherName.innerText = "jasny" : otherName.innerText = "ciemny");
+    s
+
+
 });
+
