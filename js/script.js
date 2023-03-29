12118feb27d8bdce25cd09dc7cf11d068f754826
Kacper Lewko <k.lewkosga@gmail.com>
2023-03-29 11:58:44 +0200

    reapir section

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ const buttonChangeColor = document.querySelector(".js-button");
 const body = document.querySelector(".body");
 const otherName = document.querySelector(".otherName");
 const slider = document.querySelector(".switch");
-
+const span = document.querySelector(".span");
 
 buttonChangeColor.addEventListener("click", () => {
     body.classList.toggle("body__dark");
 
     if(body.classList.contains("body__dark") ? otherName.innerText = "jasny" : otherName.innerText = "ciemny");
-    s
-
+    if(body.classList.contains("body__dark") ? span.classList.add("span--dark")) : span.classList.remove("span--dark");
 
 });
 
